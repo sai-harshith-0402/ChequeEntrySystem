3ba323da02b49d129465d0cb41c091417f674ac5
sairamyasree-eng <sai.ramyasree@imageinfosystems.com>
2026-04-28 14:57:09 +0530

    added css

--- src/main/java/com/iispl/util/Db.java
+++ src/main/java/com/iispl/util/Db.java
@@ -14,7 +14,11 @@ public class Db {
         HikariConfig cfg = new HikariConfig();
         cfg.setJdbcUrl("jdbc:postgresql://localhost:5432/Bank");
         cfg.setUsername("postgres");
+<<<<<<< Updated upstream
         cfg.setPassword("iispl660");
+=======
+        cfg.setPassword("Imageinfo@123");
+>>>>>>> Stashed changes
         cfg.setDriverClassName("org.postgresql.Driver");
 
         // Pool sizing — adjust to your DB server's max_connections
